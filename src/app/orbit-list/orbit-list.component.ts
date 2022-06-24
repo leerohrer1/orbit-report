5c0609328d57f105bb6516f31d80345b8c8cd3ac
leerohrer1 <leerohrer1@gmail.com>
2022-06-24 14:47:28 -0500

    Added error handling to satellite service.

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Satellites } from 'src/satellites';
 import { SatellitesService } from '../satellites.service';
 import { WarningDirective } from '../warning.directive';
 
@@ -10,21 +9,13 @@ import { WarningDirective } from '../warning.directive';
   styleUrls: ['./orbit-list.component.css'],
 })
 export class OrbitListComponent implements OnInit {
-  color = 'red';
   public satellites: any = [];
 
   constructor(private satelliteService: SatellitesService) {}
 
   ngOnInit(): void {
-    this.satelliteService.getSatellites()
-      .subscribe(data => this.satellites = data)
+    this.satellites = this.satelliteService
+      .getSatellites()
+      .subscribe((data) => (this.satellites = data));
   }
-
-  // isSpaceDebris(satellites: Satellite[]): boolean {
-  //   return (
-  //     satellites
-  //       .filter((e) => e.type === 'Space Debris')
-  //       .map((e) => (e.type.style.backgroundColor = 'red')).length > 0
-  //   );
-  // }
 }
--- src/app/satellites.service.ts
+++ src/app/satellites.service.ts
@@ -1,14 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { Satellites } from 'src/satellites';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SatellitesService {
+  satellites: any = this.getSatellites();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getSatellites(){
-  return this.http.get('https://handlers.education.launchcode.org/static/satellites.json');
+  getSatellites(): Observable<Satellites> {
+    const emptyArr: any = [];
+    return this.http
+      .get<Satellites>(
+        'https://handlers.education.launchcode.org/static/satellites.json'
+      )
+      .pipe(catchError(this.handleError<Satellites>('getSatellites', emptyArr)));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+  isSpaceDebris(): boolean {
+    return (
+      this.satellites.satellites
+        .filter((e: any) => e.type === 'Space Debris')
+        .map((e: any) => {
+          console.log('printing');
+          e.style.backgroundColor = 'red';
+        }).length > 0
+    );
   }
 }
